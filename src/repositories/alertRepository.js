807c5481dd2881b274d8fb6fdf9b81ae916aa002
GuiOSousa <guilherme.olivesousa@gmail.com>
2025-07-16 19:49:55 -0300

    Project Init

---
+++ src/repositories/alertRepository.js
@@ -0,0 +1,28 @@
+import { client } from "../mongo/client.js";
+
+export class AlertRepository {
+    constructor(){}
+
+    async getCollection() {
+        await client.connect();
+        const dbName = "voz-ativa";
+        const mongoClient = client.db(dbName);
+        return mongoClient.collection('alertas');
+    }
+
+    async createAlert({ estado, cidade, tipo, coordenadas, titulo, descricao, username, data} ) {
+        const collection = await getCollection();
+        await collection.insertOne({ estado, cidade, tipo, coordenadas, titulo, descricao, username, data });
+        await client.close()
+    }
+
+    async getAlerts() {
+        const collection = await this.getCollection();
+        const alerts = await collection.find().toArray();
+        //await client.close()
+        return alerts;
+    }
+
+}
+
+
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import { AlertRepository } from "./repositories/alertRepository.js";
+
+export const app = express()
+export const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World Guizin Rei deals!')
+})
+
+app.get('/alert', async (req, res) => {
+  const alertRepository = new AlertRepository()
+  const alerts = await alertRepository.getAlerts()
+  print(alerts)
+  res.send("Alerts")
+})
